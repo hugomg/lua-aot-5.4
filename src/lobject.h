aac8fb43a394f2fcf709ec0240a6a04c7d6d8860
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2021-04-26 13:25:53 -0300

    Changes to the core Lua interpreter

--- src/lobject.h
+++ src/lobject.h
@@ -533,6 +533,12 @@ typedef struct AbsLineInfo {
   int line;
 } AbsLineInfo;
 
+
+/*
+** AOT implementation
+*/
+typedef void (*AotCompiledFunction) (lua_State *L, struct CallInfo *ci);
+
 /*
 ** Function Prototypes
 */
@@ -559,6 +565,7 @@ typedef struct Proto {
   LocVar *locvars;  /* information about local variables (debug information) */
   TString  *source;  /* used for debug information */
   GCObject *gclist;
+  AotCompiledFunction aot_implementation;
 } Proto;
 
 /* }================================================================== */
