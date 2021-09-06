95e4521b0fb9d52c92916ee14685d538849819e6
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2021-09-06 02:43:44 -0300

    Remove --coro flag from benchrun
    It is now always enabled

--- scripts/bench-run.lua
+++ scripts/bench-run.lua
@@ -31,12 +31,11 @@ local benchs = {
 }
 
 local impls = {
-    { name = "jit", suffix = "",     interpreter = "luajit",        compile = false                           },
-    { name = "jof", suffix = "",     interpreter = "luajit -j off", compile = false                           },
-    { name = "lua", suffix = "",     interpreter = "../src/lua",    compile = false,                          },
-    { name = "lsw", suffix = "",     interpreter = "../src/lua-sw", compile = false,                          },
-    { name = "aot", suffix = "_aot", interpreter = "../src/lua",    compile = "../src/luaot"                  },
-    { name = "cor", suffix = "_cor", interpreter = "../src/lua",    compile = "../src/luaot --coro"           },
+    { name = "jit", suffix = "",     interpreter = "luajit",        compile = false                    },
+    { name = "jof", suffix = "",     interpreter = "luajit -j off", compile = false                    },
+    { name = "lua", suffix = "",     interpreter = "../src/lua",    compile = false,                   },
+    { name = "lsw", suffix = "",     interpreter = "../src/lua-sw", compile = false,                   },
+    { name = "aot", suffix = "_aot", interpreter = "../src/lua",    compile = "../src/luaot"           },
     { name = "trm", suffix = "_trm", interpreter = "../src/lua",    compile = "../src/luaot-trampoline --coro"},
 }
 
